f083b4c09519d53c05e61db21599c5b36e14905a
Enrique W <kique10000@gmail.com>
2013-10-12 17:31:38 -0400

    added calculation

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,4 @@
-// $(document).on("click", "table.table tr th",headerClick);
-//$(document).on("click", "table.table tr td",dataClick);
 
-                      
-// function headerClick(e) {
-//     console.log(e);
-//     $(e.currentTarget).css({
-//         color:"red"
-//     });
-// }
 var curr =  Date.today().previous().saturday();// get current date
 var context = {}
 var contactInfo = {
@@ -17,56 +8,20 @@ var contactInfo = {
     deparment:""
 }
 var payRoll = "";
-$(document).ready(function() {
-    curr = curr.addWeeks(-2);
-    /* code here */ 
-    contactInfo = $.jStorage.get("contactInfo");
 
-    getBiWeekly();
-
-    appendContact();
-
-    //Handle Listener
-    $(document).on("click", "#saveButton", saveButton);
-    $(document).on("click", "#previous", prevClick);
-    $(document).on("click", "#next", nextClick);
-
-    $("#settingsModal").keypress(function(e) {
-        var key = e.which;
-        if(key === 13){
-            $("#saveButton").click();
-        }
-   });
-});
-// var dataClick = function(e) {
-//     console.log(e);
-//     if (e.currentTarget.innerHTML != "") return;
-//     if(e.currentTarget.contentEditable != null){
-//         $(e.currentTarget).attr("contentEditable",true);
-//     }
-//     else{
-//         $(e.currentTarget).append("<input type='text'>");
-//     }    
-// }
-var dataClick = function(e) {
-    console.log(e);
-    if (e.currentTarget.innerHTML != "") return;
-    if(e.currentTarget.contentEditable != null){
-        $(e.currentTarget).attr("contentEditable",true);
-    }
-    else{
-        $(e.currentTarget).append("<input type='text'>");
-    }    
-}
-var nextClick = function(){
+var nextClick = function(e){
+    e.preventDefault();
     curr = curr.addWeeks(0);
     console.log(curr);
     getBiWeekly();
+     $('.tp1, .tp2').timepicker('setTime', '');
 }
-var prevClick = function(){
+var prevClick = function(e){
+    e.preventDefault();
     curr = curr.addWeeks(-4);
     getBiWeekly();
     console.log(curr);
+    $('.tp1, .tp2').timepicker('setTime', '');
 }
 var getBiWeekly = function(){
     var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
@@ -111,7 +66,74 @@ var appendContact = function() {
     }
 
 }
+//CALCULATE HOURS
+var calculateHrs = function(){
 
+    for(i=0; i < 14; i++){
+        $(".results").eq(i).html("");
+
+        var tp1 = $(".tp1").eq(i).val();
+        var tp2 = $(".tp2").eq(i).val();
+        // console.log(tp1.length + " " + tp2.length);
+        if(tp1.length != 0 &&  tp2.length != 0){
+            // console.log(tp1 + " " + tp2);
+            var convertedTp1 = convertTime(tp1).split(":");
+            var convertedTp2 = convertTime(tp2).split(":");
+
+          
+            if(parseInt(convertedTp2[0]) >= parseInt(convertedTp1[0]) && parseInt(convertedTp2[1]) >= parseInt(convertedTp1[1])){
+                var hrs = parseInt(convertedTp2[0]) - parseInt(convertedTp1[0]);
+                var mins;
+                if(parseInt(convertedTp2[1]) >= parseInt(convertedTp1[1])){
+                    mins = parseInt(convertedTp2[1]) - parseInt(convertedTp1[1])
+                }else{
+                    hrs = hrs - 1;
+                    mins = parseInt(convertedTp1[1]) - parseInt(convertedTp2[1]);
+                    mins = 60 - mins;
+                }
+
+
+                //meal time deduct
+                if(hrs >= 5){
+                    $(".mealTime").eq(i).html("0:30");
+                    $(".mealTime").eq(i).removeAttr("disabled");
+ 
+                    if(mins >= 30){
+                        mins = mins - 30;
+                    }else{
+                        hrs = hrs - 1;
+                        mins = 30 - mins;
+                        mins = 60 - mins;
+                    }
+                }else{
+                    $(".mealTime").eq(i).html("0:00");
+                    $(".mealTime").eq(i).attr("disabled", "disabled");
+                }
+                console.log("Result: " + sHours + ":" + sMinutes);
+                var sHours = hrs.toString();
+                var sMinutes = mins.toString();
+                if(hrs<10) sHours = "0" + sHours;
+                if(mins<10) sMinutes = "0" + sMinutes;
+                $(".results").eq(i).html(sHours + ":" + sMinutes);
+
+            }
+        }
+    }
+}
+
+var convertTime = function(time){
+    var hours = Number(time.match(/^(\d+)/)[1]);
+    var minutes = Number(time.match(/:(\d+)/)[1]);
+    var AMPM = time.match(/\s(.*)$/)[1];
+    if(AMPM == "PM" && hours<12) hours = hours+12;
+    if(AMPM == "AM" && hours==12) hours = hours-12;
+    var sHours = hours.toString();
+    var sMinutes = minutes.toString();
+    if(hours<10) sHours = "0" + sHours;
+    if(minutes<10) sMinutes = "0" + sMinutes;
+    console.log(sHours + ":" + sMinutes);
+    return sHours + ":" + sMinutes;
+}
 //TEMPLATING
 var setTemplate = function(){
     var source   = $("#tBodyContent-template").html();
@@ -119,3 +141,58 @@ var setTemplate = function(){
     var html    = template(context);
     $("#tBodyContent").html(html);
 }
+var addWeekHrs = function(){
+    var totalHrs = 0;
+    var totalMins = 0;
+    for(i=0; i < 7; i++){
+        var r = $(".results").eq(i).text();
+        if(r.length != 0){
+            var convertedTime = r.split(":");
+            totalHrs = totalHrs + parseInt(convertedTime[0]);
+            totalMins = totalMins + parseInt(convertedTime[1]);
+
+            if(totalMins >= 60){
+                totalHrs = totalHrs + 1;
+                totalMins = totalMins - 60;
+            }
+        }
+
+    }
+
+    var sTotalHours = totalHrs.toString();
+    var sTotalMinutes = totalMins.toString();
+    if(totalHrs<10) sTotalHours = "0" + sTotalHours;
+    if(totalMins<10) sTotalMinutes = "0" + sTotalMinutes;
+
+    console.log("Total HOURS: " +sTotalHours);
+    console.log("Total MINS: " +sTotalMinutes);
+
+    $(".weekTotal").eq(0).html(sTotalHours+":"+sTotalMinutes);
+}
+
+$(document).ready(function() {
+    curr = curr.addWeeks(-2);
+    /* code here */ 
+    contactInfo = $.jStorage.get("contactInfo");
+
+    getBiWeekly();
+
+    appendContact();
+
+    //Handle Listener
+    $(document).on("click", "#saveButton", saveButton);
+    $(document).on("click", "#previous", prevClick);
+    $(document).on("click", "#next", nextClick);
+    $(document).on("click", "body", calculateHrs);
+    $(document).on("click", "body", addWeekHrs);
+
+
+    $("#settingsModal").keypress(function(e) {
+        var key = e.which;
+        if(key === 13){
+            $("#saveButton").click();
+        }
+    });
+    $('.tp1, .tp2').timepicker('setTime', '');
+});
+
